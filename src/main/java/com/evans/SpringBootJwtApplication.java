45d53ff55e718a44f2bf50d0a61ce0edafd20a62
evanslangat22@gmail.com <evanslangat22@gmail.com>
2023-02-11 10:26:40 +0300

    stdout

--- src/main/java/com/evans/SpringBootJwtApplication.java
+++ src/main/java/com/evans/SpringBootJwtApplication.java
@@ -8,6 +8,7 @@ public class SpringBootJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtApplication.class, args);
+		System.out.println("Project started successfully");
 	}
 
 }
