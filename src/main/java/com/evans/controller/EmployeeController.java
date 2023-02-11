58ecd81a23cb6cecfa7e6349a0762694421f7a94
evanslangat22@gmail.com <evanslangat22@gmail.com>
2023-02-11 11:23:24 +0300

    sql

--- src/main/java/com/evans/controller/EmployeeController.java
+++ src/main/java/com/evans/controller/EmployeeController.java
@@ -1,13 +1,21 @@
 package com.evans.controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.evans.model.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @CrossOrigin()
 public class EmployeeController {
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String getEmployees() {
         return "Welcome!";
+    }
+    @GetMapping("/hi")
+    public ResponseEntity<UserDto> getUser(){
+        return getUser();
+    }
+    @GetMapping("/hello")
+    public String getUsers(){
+        return "hello evans";
     }
 }
--- src/main/java/com/evans/service/JwtUserDetailsService.java
+++ src/main/java/com/evans/service/JwtUserDetailsService.java
@@ -36,4 +36,9 @@ public class JwtUserDetailsService implements UserDetailsService {
 		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
 		return userDao.save(newUser);
 	}
+	public UserDao getUser(UserDto user){
+		UserDao newuser=new UserDao();
+		newuser.setUsername("kiprono langat");
+		return newuser;
+	}
 }
